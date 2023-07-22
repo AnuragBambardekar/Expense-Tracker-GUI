44c4379096dbcd009ef5d1cdb33d97c5038f49d3
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-07-22 16:52:28 -0400

    created components for the app

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Expense Tracker App
 
+## Project Logs
 - ```npx create-react-app expense-tracker-app```
-
+- Installed the VSCode Extension: ES7 React/Redux/GraphQL/React-Native snippets
+- create components folder inside src -> create Header.js file
+- type 'rafc' in header.js -> it will give us a functional component to work with
+- Import Header.js in App.js
+- Create rest of the components and include it in App.js
+- Add extension: React DevTools in Chrome to inspect React Components
--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Header/>
+      <div className='container'>
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
     </div>
   );
 }
