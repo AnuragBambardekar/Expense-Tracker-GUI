be6c349c26fd327af4379f62d09cb88cfd8f687c
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-07-25 00:10:18 -0400

    IncomeExpenses and Balances script updated with context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Balance from './components/Balance'
-import IncomeExpenses from './components/IncomeExpenses';
+import IncomeExpenses from './components/IncomeExpenses'
 import TransactionList from './components/TransactionList';
 import AddTransaction from './components/AddTransaction';
 
