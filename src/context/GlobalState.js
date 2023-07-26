bdf30815049bd043373ecd1d0632137d67a08a1d
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-07-26 00:04:50 -0400

    delete a transaction

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -27,8 +27,17 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    // actions that make calls to reducer
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
     return (<GlobalContext.Provider value={{
-        transactions: state.transactions
+        transactions: state.transactions,
+        deleteTransaction
     }}>
         {children}
     </GlobalContext.Provider>)
