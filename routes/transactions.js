b5ae55a20437e9b8b1335d5574deb4e74e999713
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-08-11 15:54:37 -0400

    route for editing transactions added

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getTransactions, addTransactions, deleteTransactions } = require('../controllers/transactions')
+const { getTransactions, addTransactions, deleteTransactions, editTransactions } = require('../controllers/transactions')
 
 // router.get('/',(req,res) => res.send('Hello'))
 router
@@ -10,6 +10,7 @@ router
 
 router
     .route('/:id')
-    .delete(deleteTransactions);
+    .delete(deleteTransactions)
+    .put(editTransactions);
 
 module.exports = router;
